3ac15b157ec47e15118593e9527c36b643b53701
fix it <fix.it.central2021@gmail.com>
2021-07-10 21:00:55 -0600

    configs

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 const express = require("express");
 const app = express();
+var cors = require('cors')
 app.use(express.json());
 
 
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
 
 
 const ordenesRoute = require('./routes/orden')
@@ -12,9 +18,11 @@ const repuestosRoute = require('./routes/repuestos')
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,access-token,access-user");
-    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE , PUT");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE , PUT, OPTIONS");
     next();
   });
+
+  
   
 app.use('/repuesto',repuestosRoute)
 app.use('/orden',ordenesRoute)
